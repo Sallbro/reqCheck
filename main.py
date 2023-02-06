db074beec987c4167c2283ef83988d43b26115e2
salman <asusalman986@gmail.com>
2023-02-06 17:15:09 +0530

    try to resolve exception case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scraper import get_requirements
-from flask import Flask
+from flask import Flask, render_template, request,redirect, url_for
 import requests
 
 app = Flask(__name__)
@@ -7,14 +7,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def lol():
-    return "lol"
+    return render_template('search.html')
 
 
-@app.route("/get/")
+@app.route("/get", methods=['POST', 'GET'])
 def get():
-    url = "https://store.steampowered.com/search/suggest?term=spo&f=games&cc=IN&realm=1&l=english&v=17639006&excluded_content_descriptors%5B%5D=3&excluded_content_descriptors%5B%5D=4&use_store_query=1&use_search_spellcheck=1"
-    res = get_requirements(url)
-    return res
+    if request.method == 'POST':
+        print(request.form)
+        search_val = request.form["search"]
+        print("search_val:", search_val)
+        # url_test = "https://store.steampowered.com/search/suggest?term=spo&f=games&cc=IN&realm=1&l=english&v=17639006&excluded_content_descriptors%5B%5D=3&excluded_content_descriptors%5B%5D=4&use_store_query=1&use_search_spellcheck=1"
+        url = 'https://store.steampowered.com/search/suggest?term=%s' % search_val + \
+            '&f=games&cc=IN&realm=1&l=english&v=17639006&excluded_content_descriptors%5B%5D=3&excluded_content_descriptors%5B%5D=4&use_store_query=1&use_search_spellcheck=1'
+        res = get_requirements(url)
+        return res
+    else:
+        return redirect(url_for('lol'))
 
 
 if __name__ == '__main__':
