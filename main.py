2693df211fa0c523283a5ed4b2da9084f689702d
salman <asusalman986@gmail.com>
2023-02-06 12:45:10 +0530

    succefully able to fetch user search data and findout exception case

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-from scraper import get_requirements;
+from scraper import get_requirements
 from flask import Flask
 import requests
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def lol():
     return "lol"
 
 
-@app.route("/get/<>")
+@app.route("/get/")
 def get():
-    res = get_requirements("stardew");
+    url = "https://store.steampowered.com/search/suggest?term=spo&f=games&cc=IN&realm=1&l=english&v=17639006&excluded_content_descriptors%5B%5D=3&excluded_content_descriptors%5B%5D=4&use_store_query=1&use_search_spellcheck=1"
+    res = get_requirements(url)
     return res
 
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
 
 
 # https://store.steampowered.com/search/suggest?term=str&f=games&cc=US&realm=1&l=english&v=17631244&excluded_content_descriptors[]=3&excluded_content_descriptors[]=4&use_store_query=1&use_search_spellcheck=1
--- scraper.py
+++ scraper.py
@@ -2,44 +2,49 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def get_url()
-    page = requests.get("https://store.steampowered.com/search/suggest?term=str&f=games&cc=US&realm=1&l=english&v=17631244&excluded_content_descriptors[]=3&excluded_content_descriptors[]=4&use_store_query=1&use_search_spellcheck=1")
+def get_url(suggestion_url):
+    # page = requests.get("https://store.steampowered.com/search/suggest?term=spo&f=games&cc=IN&realm=1&l=english&v=17639006&excluded_content_descriptors%5B%5D=3&excluded_content_descriptors%5B%5D=4&use_store_query=1&use_search_spellcheck=1");
+    page = requests.get(suggestion_url)
+
     results = BeautifulSoup(page.content, "html.parser")
-    url = results.find("a")
+    # print("res: ",results);
+    url_attribute = results.find("a")
+    url = url_attribute.get("href")
+    print("\n url: ", url)
+
     return url
 
 
-def get_requirements(args):
-    minVal={};
-    recVal={};
+def get_requirements(suggestion_url):
+    minVal = {}
+    recVal = {}
     print("LOLLLLLLLL:-----------------------------")
-    page = requests.get(get_url(args));
-    print(page.content)
-    results = BeautifulSoup(page.content, "html.parser");
-    print("min...");
-    minimumReq = results.find_all("div",{"class":"game_area_sys_req_leftCol"});
+    page = requests.get(get_url(suggestion_url))
+    results = BeautifulSoup(page.content, "html.parser")
+    # print(results.prettify());
+
+    print("\n minimum...")
+    minimumReq = results.find_all(
+        "div", {"class": "game_area_sys_req_leftCol"})
     for thing in minimumReq:
         i = thing.find_all("li")
-        # lol = [print(x.get_text()) for x in i]
         for x in i:
-            if(len(x.get_text().split(":")) >1):
+            if(len(x.get_text().split(":")) > 1):
                 key = x.get_text().split(":")[0]
                 value = x.get_text().split(":")[1]
                 minVal[key] = value
         print(minVal)
 
-
-    # print(results.prettify());
-    print("\nrecomdedReq...");
-    recomdedReq = results.find_all("div",{"class":"game_area_sys_req_rightCol"});
+    print("\n recomdedReq...")
+    recomdedReq = results.find_all(
+        "div", {"class": "game_area_sys_req_rightCol"})
     for thing in recomdedReq:
         i = thing.find_all("li")
-        # lol = [print(x.get_text()) for x in i]
         for x in i:
-            if(len(x.get_text().split(":")) >1):
+            if(len(x.get_text().split(":")) > 1):
                 key = x.get_text().split(":")[0]
                 value = x.get_text().split(":")[1]
                 recVal[key] = value
         print(recVal)
 
-    return {"min":minVal, "recommended": recVal};
+    return {"min": minVal, "recommended": recVal}
