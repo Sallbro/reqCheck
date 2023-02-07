fbf3817b067e8b7be913cba484ee3b0b24854890
salman <asusalman986@gmail.com>
2023-02-07 18:50:03 +0530

    display the data in UI

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from scraper import get_requirements
-from flask import Flask, render_template, request,redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 
 app = Flask(__name__)
+
+
+# def result(res):
+#     print("res func", res)
+#     return render_template("result.html", res=res)
 
 
 @app.route("/")
@@ -13,16 +18,13 @@ def lol():
 @app.route("/get", methods=['POST', 'GET'])
 def get():
     if request.method == 'POST':
-        print(request.form)
         search_val = request.form["search"]
-        print("search_val:", search_val)
-        # url_test = "https://store.steampowered.com/search/suggest?term=spo&f=games&cc=IN&realm=1&l=english&v=17639006&excluded_content_descriptors%5B%5D=3&excluded_content_descriptors%5B%5D=4&use_store_query=1&use_search_spellcheck=1"
         url = 'https://store.steampowered.com/search/suggest?term=%s' % search_val + \
             '&f=games&cc=IN&realm=1&l=english&v=17639006&excluded_content_descriptors%5B%5D=3&excluded_content_descriptors%5B%5D=4&use_store_query=1&use_search_spellcheck=1'
         res = get_requirements(url)
-        return res
+        return render_template('result.html', res=res);
     else:
-        return redirect(url_for('lol'))
+        return redirect(url_for('lol'));
 
 
 if __name__ == '__main__':
@@ -30,3 +32,4 @@ if __name__ == '__main__':
 
 
 # https://store.steampowered.com/search/suggest?term=str&f=games&cc=US&realm=1&l=english&v=17631244&excluded_content_descriptors[]=3&excluded_content_descriptors[]=4&use_store_query=1&use_search_spellcheck=1
+# https://store.steampowered.com/search/suggest?term=spo&f=games&cc=IN&realm=1&l=english&v=17639006&excluded_content_descriptors%5B%5D=3&excluded_content_descriptors%5B%5D=4&use_store_query=1&use_search_spellcheck=1
